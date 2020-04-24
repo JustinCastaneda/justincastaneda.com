7e8877a72a1aa98ed04a0943a06ab64239806dfe
Justin Castaneda <justincastaneda@Justins-MacBook-Pro.local>
2020-04-23 21:45:09 -0700

    New Gatsby Install

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>A New Site is coming!</h1>
+    <p>
+      I'm working on a big site refresh right now, but I will be back shortly.
+    </p>
+  </Layout>
+);
+
+export default IndexPage;
