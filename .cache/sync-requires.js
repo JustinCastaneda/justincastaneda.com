ef34cdfc8fe2913d4d56a7d5d9bec494ea968ca0
Justin Castaneda <justincastaneda@Justins-MacBook-Pro.local>
2020-05-04 17:05:12 -0700

    Adding a new jc favicon

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/justincastaneda/Documents/justincastaneda.com/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/justincastaneda/Documents/justincastaneda.com/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/justincastaneda/Documents/justincastaneda.com/src/pages/index.js"))),
   "component---src-pages-page-2-js": hot(preferDefault(require("/Users/justincastaneda/Documents/justincastaneda.com/src/pages/page-2.js")))
